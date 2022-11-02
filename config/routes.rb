a6884f72d446ea66f332008a0c0ec27bc2c5daea
rich <adukyerer@gmail.com>
2022-11-02 08:12:22 +0000

    Add files for backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-namespace :api do
+  namespace :api do
     namespace :v1 do
       get 'greetings/random', to: 'greeting#index'
     end
   end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
